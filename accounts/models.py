9f65e779609324ae2671f7df5768136365521ed6
Ira Macazo <ira_macazo@dlsu.edu.ph>
2017-07-06 23:38:10 +0800

    Added User Account Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    company = models.CharField(max_length=50)
+    USER_TYPE_CHOICES = (
+        ('FI', 'Finance'),
+        ('EM', 'Equipment Manager'),
+        ('CU', 'Customer'))
+    user_type = models.CharField(max_length=2, choices=USER_TYPE_CHOICES)
