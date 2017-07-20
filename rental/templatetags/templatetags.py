f722b19db01507d1433a0df49b2f3ccb76443d4e
Ira Macazo <ira_macazo@dlsu.edu.ph>
2017-07-21 01:08:46 +0800

    Added fields in the checkout form (incomplete)

--- rental/templatetags/templatetags.py
+++ rental/templatetags/templatetags.py
@@ -10,3 +10,9 @@ def get_cart(request):
     cart = request.session.get('cart', [])
     equipment = Equipment.objects.filter(id__in=cart)
     return {'cart': equipment}
+
+
+@register.assignment_tag()
+def cart_length(request):
+    cart = request.session.get('cart', [])
+    return len(cart)
--- static/js/login.js
+++ static/js/login.js
@@ -35,19 +35,6 @@ $(document).ready(function () {
 
     /** CSRF FOR AJAX **/
 
-    //Attaching event handler to .dropdown selector.
-    $('.dropdown').on({
-
-      //fires after dropdown is shown instance method is called.(if you click //anywhere else)
-        "shown.bs.dropdown": function() { this.close= false; },
-
-      //when dropdown is clicked
-        "click": function() { this.close= true; },
-
-     //when close event is triggered
-        "hide.bs.dropdown":  function() { return this.close; }
-    });
-
     $("#login").click(function()
     {
         var username = $("#login_username_input").val();
