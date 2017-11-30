30ec279ff74be5f88856e46439dc8a26dc4a118a
jrenzodl <30580632+jrenzodl@users.noreply.github.com>
2017-12-01 06:52:45 +0800

    Update tests.py
    TEST

--- equipment/tests.py
+++ equipment/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import Client
+from .forms import *   # import all forms
 
-# Create your tests here.
+class Setup_Class(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="user@mp.com", password="user", first_name="user", phone=12345678)
+
+class User_Form_Test(TestCase):
+
+    # Valid Form Data
+    def test_UserForm_valid(self):
+        form = UserForm(data={'email': "user@mp.com", 'password': "user", 'first_name': "user", 'phone': 12345678})
+        self.assertTrue(form.is_valid())
+
+    # Invalid Form Data
+    def test_UserForm_invalid(self):
+        form = UserForm(data={'email': "", 'password': "mp", 'first_name': "mp", 'phone': ""})
+        self.assertFalse(form.is_valid())
