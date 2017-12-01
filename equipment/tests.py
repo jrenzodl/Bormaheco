a0c676fc92d2b8653a813fc1bb6448dc93995bda
jrenzodl <30580632+jrenzodl@users.noreply.github.com>
2017-12-01 08:09:35 +0800

    Update tests.py

--- equipment/tests.py
+++ equipment/tests.py
@@ -1,6 +1,12 @@
-from django.test import SmipleTestCase, TestCase
-from django.contrib.admin.models import ADDITION, CHANGE, DELETION
-from .forms import *   # import all forms
-from django.contrib.auth.models import User
-from django.test.utils *
-from .models import *
+from django.test import TestCase
+
+
+class ExampleTestCase(TestCase):
+    def setUp(self):
+        self.maven = Dog(name="Maven", breed="corgi")
+        self.maven.save()
+
+    def test_save(self):
+        self.assertEquals(self.maven.name, "Maven")
+        # You can mix in pytest's `assert` approach!
+        assert self.maven.breed == "corgi
